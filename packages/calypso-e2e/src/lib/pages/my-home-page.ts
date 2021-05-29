2f857254b26cb57b82d9357e61a8837c93fcfa39
Edwin Takahashi <egao@outlook.com>
2021-05-28 22:34:42 -0700

    Migrate additional test cases from wp-like.

--- packages/calypso-e2e/src/lib/pages/my-home-page.ts
+++ packages/calypso-e2e/src/lib/pages/my-home-page.ts
@@ -11,10 +11,33 @@ import { Page } from 'playwright';
 const selectors = {
 	dashboard: '.customer-home__main',
 	statsCard: '.stats',
+	visitSiteButton: 'text=Visit site',
 };
 
+/**
+ * Page representing the WPCOM home dashboard.
+ *
+ * @augments {BaseContainer}
+ */
 export class MyHomePage extends BaseContainer {
 	constructor( page: Page ) {
 		super( page, selectors.dashboard );
+	}
+
+	async _postInit(): Promise< void > {
+		await this.page.waitForSelector( selectors.statsCard );
+	}
+
+	/**
+	 * Click on the Visit Site button on the home dashboard.
+	 *
+	 * @returns {Promise<void>} No return value.
+	 */
+	async visitSite(): Promise< void > {
+		await this.page.waitForSelector( selectors.visitSiteButton );
+		await Promise.all( [
+			this.page.waitForNavigation(),
+			this.page.click( selectors.visitSiteButton ),
+		] );
 	}
 }
